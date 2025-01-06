66aa8dced8e72268cebe0b639c6674072d8fef80
Sayyid Mehdi Bin Safiyullah <sayyidmehdi@Sayyids-MacBook-Air.local>
2025-01-06 14:30:21 +0800

    Test merge

--- 1SMARTCONTRACT/contracts/CarbonCreditToken.sol
+++ 1SMARTCONTRACT/contracts/CarbonCreditToken.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+
+// Test Merge
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
